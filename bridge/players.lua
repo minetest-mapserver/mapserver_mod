4e0d68e7c009a535a22f115089f25563a6f8bbac
NatureFreshMilk <naturefreshmilk@github.com>
2019-05-15 16:08:59 +0200

    player.moderator flag in bridge

--- bridge/players.lua
+++ bridge/players.lua
@@ -6,13 +6,15 @@ mapserver.bridge.add_players = function(data)
   for _, player in ipairs(minetest.get_connected_players()) do
 
     local is_hidden = minetest.check_player_privs(player:get_player_name(), {mapserver_hide_player = true})
+    local is_moderator = minetest.check_player_privs(player:get_player_name(), {ban = true})
 
     local info = {
       name = player:get_player_name(),
       pos = player:get_pos(),
       hp = player:get_hp(),
       breath = player:get_breath(),
-      velocity = player:get_player_velocity()
+      velocity = player:get_player_velocity(),
+      moderator = is_moderator
     }
 
     if not is_hidden then
