8ca103b7f01efd318f545f3ed0247f6500467537
NatureFreshMilk <naturefreshmilk@github.com>
2019-06-14 07:08:50 +0200

    player rtt, protocol version

--- bridge/players.lua
+++ bridge/players.lua
@@ -8,13 +8,25 @@ mapserver.bridge.add_players = function(data)
     local is_hidden = minetest.check_player_privs(player:get_player_name(), {mapserver_hide_player = true})
     local is_moderator = minetest.check_player_privs(player:get_player_name(), {ban = true})
 
+    local detail = minetest.get_player_information(player:get_player_name())
+
+    local protocol_version = -1
+    local rtt = -1
+
+    if detail then
+      rtt = detail.avg_rtt
+      protocol_version = detail.protocol_version
+    end
+
     local info = {
       name = player:get_player_name(),
       pos = player:get_pos(),
       hp = player:get_hp(),
       breath = player:get_breath(),
       velocity = player:get_player_velocity(),
-      moderator = is_moderator
+      moderator = is_moderator,
+      rtt = rtt,
+      protocol_version = protocol_version
     }
 
     if not is_hidden then
