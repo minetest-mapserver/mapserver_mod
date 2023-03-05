03958d05c07d3ad877d0478b6a16d747b6c08cf2
Niklp <89982526+Niklp09@users.noreply.github.com>
2023-03-05 15:38:01 +0100

    fix max lag detection (#28)
    * fix max lag detection
    
    * restore 5.4 support

--- bridge/defaults.lua
+++ bridge/defaults.lua
@@ -1,20 +1,30 @@
 
+local has_get_server_max_lag
+
+if type(minetest.get_server_max_lag) == "function" then
+  has_get_server_max_lag = true
+else
+  has_get_server_max_lag = false
+end
+
 local function explode(sep, input)
-        local t={}
-        local i=0
-        for k in string.gmatch(input,"([^"..sep.."]+)") do
-            t[i]=k
-            i=i+1
-        end
-        return t
+  local t={}
+  local i=0
+  for k in string.gmatch(input,"([^"..sep.."]+)") do
+      t[i]=k
+      i=i+1
+  end
+  return t
 end
 
 local function get_max_lag()
-        local arrayoutput = explode(", ",minetest.get_server_status())
-        arrayoutput = explode("=",arrayoutput[4])
-        return arrayoutput[1]
+  if has_get_server_max_lag then
+    return minetest.get_server_max_lag()
+  end
+  local arrayoutput = explode(", ",minetest.get_server_status())
+  arrayoutput = explode("=",arrayoutput[4])
+  return arrayoutput[1]
 end
-
 
 mapserver.bridge.add_defaults = function(data)
   data.time = minetest.get_timeofday() * 24000
